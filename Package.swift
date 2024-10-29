b4aa5eb3ea4f4514445690b6ed813f596eb1a4f9
atomic-mehdi <125278614+atomic-mehdi@users.noreply.github.com>
2024-10-29 19:44:43 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
    name: "shared",
    platforms: [
-     .iOS(.v16),
+     .iOS(.v14),
    ],
    products: [
       .library(name: "shared", targets: ["shared"])
