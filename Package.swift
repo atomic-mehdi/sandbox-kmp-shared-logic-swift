7e34f281ea0521b96a48328004ca4225cbd4efe8
atomic-mehdi <125278614+atomic-mehdi@users.noreply.github.com>
2024-10-29 19:34:04 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "shared",
-         url: "https://github.com/atomic-mehdi/sandbox-kmp-shared-logic/releases/download/1.0.0/shared.xcframework.zip",
+         url: "https://github.com/atomic-mehdi/sandbox-kmp-shared-logic/releases/download/1.0.1/shared.xcframework.zip",
          checksum:"a1c811d277d813e623ae821d66a68daa9393b4f11c35f2c9dd70afe252b1e565")
    ]
 )
